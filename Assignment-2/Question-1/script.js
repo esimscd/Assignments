f37c0f9ffb462c57e308112077e40073a67ba634
esimp222 <elizabethsimpson96@outlook.com>
2025-02-08 13:12:18 +0400

    Updated JavaScript, loop added to list array of destination options

--- Assignment-2/Question-1/script.js
+++ Assignment-2/Question-1/script.js
@@ -1,5 +1,5 @@
-//The purpose of this web app is to generate a list of travel destination/activities
-
+//The purpose of this web app is to generate a list of travel destination/activities based on two inputs from the user.
+//The app uses booleans to check all conbinations of responses and display an array of results and responds to event of button click to run
 
 //Accessing input from page, selecting all radio input buttons using ID
 const hot = document.getElementById("hot")
@@ -11,38 +11,47 @@ const relax = document.getElementById("relax")
 let resultDisplay = document.getElementById("result")
 
 //Creating an object with an four arrays inside of possible destinations to select from
-
-let hotAdventure = ["Visit Ranthambore National Park in India", "Safari in Kenya", "Scuba Diving in the Galapagos Islands" ]
-let hotRelax = ["Relax in the Maldives", "Bora Bora", "Explore the island of Maurtius"]
-let coldAdventure = ["Hiking in Patagonia", "Voyage in Antartica", "Volcano Hiking in Russia" ]
-let coldRelaxing = ["A weekend break in Halstatt, Austria", "City break in Vancouver, Canada"]
-
+const destinations = { 
+    hotAdventure: ["Visiting Ranthambore National Park in India", "Safari in Kenya", "Scuba Diving in the Galapagos Islands" ],
+    hotRelax: ["Island hopping in the Maldives", "A boat trip in Bora Bora", "Explore the islands of Maurtius"],
+    coldAdventure: ["Skiing in Hokkaido", "Expedition to Antartica", "Volcano Hiking in Russia" ],
+    coldRelaxing: ["A Secluded countryside stay in Norway", "City break in Vancouver", "Visiting the lagoons in Iceland"]
+}
 
 //Function using boolean and if else to select a destination randomly from a selected array
 function destinationSelector() {
     console.log("button works and function runs")//Checking button to run function
 
+    let selectedArray = [];
+
     if (hot.checked && adventure.checked){
-        resultDisplay.innerText = "hot and adventure"
+        selectedArray = destinations.hotAdventure
     }
 
     else if (hot.checked && relax.checked) {
-        resultDisplay.innerText = "hot and relax"
+        selectedArray = destinations.hotRelax
     }
 
     else if (cold.checked && adventure.checked) {
-        resultDisplay.innerText = "cold and adventure"
+        selectedArray = destinations.coldAdventure
     }
     else if (cold.checked && relax.checked) {
-        resultDisplay.innerText = "cold and relax"
+        selectedArray = destinations.coldRelaxing
     }
     else {
-        alert("Please select an option for each question")
+        alert("Please select an option for each question")//Alert if either question does not have a response selected
     }
 
-    //
+    //Displaying list of destinations from array as undordered list
+    resultDisplay.innerHTML = "How about: <ul>";
 
+    // Using a for loop to iterate through the array and add each item as a list element
+    for (let i = 0; i < selectedArray.length; i++) {
+        resultDisplay.innerHTML += `<li>${selectedArray[i]}</li>`;
+    }
+
+    resultDisplay.innerHTML += "</ul>"; 
 }
 
 //Selecting button and defining event
-document.getElementById("checkInputBtn").addEventListener("click", destinationSelector)
+document.getElementById("checkInputBtn").addEventListener("click", destinationSelector);
