a9d0bf827f72f6c3af5e851a2c3b5e70d62b67c4
esimp222 <elizabethsimpson96@outlook.com>
2025-03-10 19:02:56 +0000

    End points tested and documentation complete

--- Assignment-4/index.js
+++ Assignment-4/index.js
@@ -15,23 +15,9 @@ const pool = mysql.createPool({
     queueLimit: 0 
 });
 
-console.log(process.env.USER_ID);
-
 //Check the server is running
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
-
-//Upload an item
-app.post('/upload-item', (req,res) => {
-    const { name, price, size, item_cond } = req.body
-    const sql = 'INSERT INTO sale_items (name, price, size, item_cond) VALUES (?, ?, ?, ?)'
-    pool.query(sql, [name, price, size, item_cond], (err, result) => {
-        if (err) {
-            return res.status(500).json({error: `${err}`})
-        }
-        res.status(201).json({id:result.insertId, message: `${name} uploaded successfully`})
-    })
 });
 
 //Get request to show all currently listed items
@@ -43,25 +29,6 @@ app.get('/items', (req, res) => {
         }
         return res.status(200).json(results)
     })
-});
-
-//Put request to update price on a listed item 
-app.put('/update-price/:id', (req, res) => {
-    const itemId = req.params.id
-    const { price } = Number(req.body)
-    if (Number.isNaN(price) || price <= 0){
-        return res.status(400).json({error: 'Price must be a number over zero'});
-    } // Check price input by user is a number above zero before running sql query 
-    const sql = 'UPDATE sale_items set price = ? WHERE id = ?'
-    pool.query(sql, [price, itemId], (err, result) => {
-        if (err) {
-            return res.status(500).json({error: `${err}`});
-        }
-        if (result.affectedRows === 0) {
-            return res.status(404).json({error: 'Item does not exist'});
-        }
-        res.status(200).json({message: `Price successfully updated to ${price} on item with ID: ${itemId}`})
-    } )
 });
 
 //Get request for full info on a single item by Id
@@ -79,7 +46,41 @@ app.get('/items/:id', (req, res) => {
     })
 });
 
-//Delte request to remove single item by Id
+//Upload an item
+app.post('/upload-item', (req,res) => {
+    const { name, price, size, item_cond } = req.body
+    if (!name || !price || !size || !item_cond) {
+        return res.status(400).json({error: 'All fields are required'})
+    }
+    const sql = 'INSERT INTO sale_items (name, price, size, item_cond) VALUES (?, ?, ?, ?)'
+    pool.query(sql, [name, price, size, item_cond], (err, result) => {
+        if (err) {
+            return res.status(500).json({error: `${err}`})
+        }
+        res.status(201).json({id:result.insertId, message: `${name} uploaded successfully`})
+    })
+});
+
+//Put request to update price on a listed item 
+app.put('/update-price/:id', (req, res) => {
+    const itemId = req.params.id
+    const { price } = req.body
+    if (typeof price !== "number" || price <= 0){
+        return res.status(400).json({error: 'Price must be a number over zero'});
+    } // Check price input by user is a number above zero before running sql query 
+    const sql = 'UPDATE sale_items set price = ? WHERE id = ?'
+    pool.query(sql, [price, itemId], (err, result) => {
+        if (err) {
+            return res.status(500).json({error: `${err}`});
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({error: 'Item does not exist'});
+        }
+        res.status(200).json({message: `Price successfully updated to ${price} on item with ID: ${itemId}`})
+    } )
+});
+
+//Delete request to remove single item by Id
 app.delete('/delete-item/:id', (req, res) => {
     const itemId = req.params.id
     const sql = 'DELETE FROM sale_items WHERE id = ?'
